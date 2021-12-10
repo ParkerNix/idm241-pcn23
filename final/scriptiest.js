ba5264471ee1200ce842f30e365eccdc2e6ab381
Parker Nix <71665527+ParkerNix@users.noreply.github.com>
2021-12-10 04:48:18 -0500

    Download fades out

--- final/scriptiest.js
+++ final/scriptiest.js
@@ -1,13 +1,34 @@
-document.addEventListener(onload, uncheck(), false);
+document.addEventListener(onload, bye(), false);
 
-function uncheck() {
+function bye() {
     document.querySelector("#ms\\-download").checked = false;
     document.querySelector("#ms\\-container").style.display = "none";
 }
+
+function myMove() {
+    let id = null;
+    const elem = document.querySelector("#ms-container");
+    let pos = 100;
+    clearInterval(id);
+    id = setInterval(frame, 1);
+    function frame() {
+        if (pos == 0) {
+            clearInterval(id);
+            elem.style.display = "none";
+            document.querySelector("#ms\\-download").checked = false;
+        } else {
+            pos--;
+            elem.style.opacity = pos + '%';
+        }
+    }
+}
+
 
 function func1(event) {
     console.log("working");
     document.querySelector("#ms\\-download").checked = true;
     document.querySelector("#ms\\-container").style.display = "block";
+    document.querySelector("#ms\\-container").style.opacity = "100%";
     console.log("downloaded file!");
+    window.setTimeout(myMove, 3500);
 }
